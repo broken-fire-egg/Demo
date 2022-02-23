cfed7cde5970fabb05223644400e5179929fc8fc
MurRuck <53210642+MurRuck@users.noreply.github.com>
2022-02-23 16:47:26 +0900

    Add files via upload

---
+++ Assets/Scripts/AvoidState.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidState : State
+{
+    Vector3 v;
+    public GameObject hero;
+    public int t = 0;
+    public Vector3 detectedBulletDirection = Vector3.zero;
+    public Transform b;
+    Vector3 prev;
+
+    public override State RunCurrentState()
+    {
+        sManager.avoiding = true;
+
+        
+
+
+        if (t < 5)
+            return this;
+        else
+        {
+            t = 0;
+            detectedBulletDirection = Vector3.zero;
+            b = null;
+            prev = Vector3.zero;
+            sManager.avoidCurrentCooltime = sManager.avoidCooltime;
+            return nextState[0];
+        }
+    }
+    private void Update()
+    {
+        if (sManager.currentState == this && sManager.detectedBullets.Count > 0)
+        {
+            if (prev == Vector3.zero)
+            {
+                b = sManager.detectedBullets[0];
+                prev = b.position;
+                return;
+            }
+            else
+            {
+                detectedBulletDirection = b.position - prev;
+                prev = b.position;
+            }
+            if (sManager.detectedBullets[0].gameObject.activeInHierarchy && t < 5)
+            {
+                if (sex(hero) - sManager.detectedBullets[0].gameObject.transform.eulerAngles.z > 0)
+                {
+                    if (sex(hero) > sManager.detectedBullets[0].gameObject.transform.eulerAngles.z)
+                        sManager.subject.transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3)Vector2.Perpendicular(transform.position - Hero.instance.transform.position) * 3, 0.15f);//아래로
+                    else
+                        sManager.subject.transform.position = Vector3.MoveTowards(transform.position, transform.position - (Vector3)Vector2.Perpendicular(transform.position - Hero.instance.transform.position) * 3, 0.15f);//아래로
+                }
+                else
+                    sManager.subject.transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3)Vector2.Perpendicular(transform.position - Hero.instance.transform.position) * 3, 0.15f);//아래로
+            }
+            t++;
+        }
+    }
+
+    private float sex(GameObject her)
+    {
+        Vector3 v = gameObject.transform.position - her.transform.position;
+        return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,19 +6,27 @@ public class Enemy : MonoBehaviour
 {
     public float hp;
     public float maxhp;
+    public float knockBackTimer;
     public Transform hpBar;
+
+    float timer;
+    public bool a = false;
+    Vector2 test;
 
     void Hit(float damage)
     {
         hp -= damage;
         if (hp <= 0)
             Destroy(gameObject);
-        hpBar.localScale = new Vector3(1, hp/maxhp, 1);
+        hpBar.localScale = new Vector3(1, hp / maxhp, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        if (a == true && knockBackTimer <= timer)
+            backStop();
         //Vector3 dir = Hero.instance.transform.position - transform.position;
         //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
@@ -26,10 +34,20 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("PlayerBullet"))
+        if (collision.gameObject.CompareTag("PlayerBullet"))
         {
+            backStop();
+            GetComponent<Rigidbody2D>().AddForce(collision.gameObject.GetComponent<Rigidbody2D>().velocity * 100f);
+            timer = 0;
             Hit(collision.gameObject.GetComponent<HeroBullet>().damage);
             collision.gameObject.SetActive(false);
+            a = true;
         }
+    }
+
+    void backStop()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        a = false;
     }
 }
